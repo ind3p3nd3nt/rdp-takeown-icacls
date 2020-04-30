0133d8197f73034329dd3f30c5676f142da310f0
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-30 10:03:45 -0400

    Made for resetting acl configurations

---
+++ reset.sh
@@ -0,0 +1,55 @@
+# GITHUB REPO: https://github.com/independentcod/rdp-takeown-icacls
+# -
+# This bash script will configure Windows to allow and deny two different person on the computer from accessing/executing each others data.
+# I had this idea when people were starting to screw with my remote desktop server.
+# INSTALLATION: You will need git-bash to run this script.
+#               Then you just configure it the way you want it and execute it.
+############
+#  CONFIG  #
+############
+file="list.txt"
+directory="C:\\Windows\\System32\\"
+denyuser="ADMIN"
+grantuser="Administrator"
+quote='"'
+rpar=")"
+lpar="("
+d2=":"
+############
+# Clearing old used files.
+rm $file;
+rm Commandstorun.log;
+# Listing files and folders in directory and storing into file
+ls $directory >$file;
+# Reading File begins.
+while IFS= read -r line
+do
+# If line contains a file, use proper formatting.
+if [[ "$line" == *"."* ]]; then
+line="*$line*"
+# Same thing goes if it is a directory.   
+else
+line="$line\\"
+fi
+# Setting up strings concatenations
+dirstring="$quote"
+dirstring+="$directory"
+dirstring+="$line"
+dirstring+="$quote"
+# Taking owner ship of the file/directory
+ps1="takeown.exe /D Y /A /R /F $dirstring"
+# reset permissions to all
+ps2="icacls.exe $dirstring /setowner $grantuser /T /Q /C"
+ps3="icacls.exe $dirstring /reset /T /Q /C"
+# Write commands into log file.
+echo $ps1 >>Commandstorun.log;
+echo $ps2 >>Commandstorun.log;
+echo $ps3 >>Commandstorun.log;
+done <"$file"
+# Display commands.
+cat Commandstorun.log;
+# Ask for confirm to run the script
+read -p 'If this looks OK, you may want to execute now? (Y)' choice
+case $choice in
+ Y) sh Commandstorun.log && echo 'I hope it was not too hard ;o)';
+esac

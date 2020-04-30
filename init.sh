9193d9357cf2f28a1630f47651e70a22ff6dedf3
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-04-30 07:29:24 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -60,10 +60,10 @@ grantuserstring+="$rpar$quote"
 ps1="takeown.exe /D Y /A /R /F $dirstring"
 # Giving Full permission to grantuser
 ps2="icacls.exe $dirstring /setowner $grantuser /T /Q /C"
-ps3="icacls.exe $dirstring /inheritancelevel:e /grant:r $targetgrantobjectstring /T /Q /C"
+ps3="icacls.exe $dirstring /grant:r $targetgrantobjectstring /T /Q /C"
 # Revoking other access
 ps4="icacls.exe $dirstring /deny $denyuserstring $targetdenyobjectstring /T /Q /C"
-ps5="icacls.exe $dirstring /remove:g $targetdenyobjectstring /T /Q /C"
+ps5="icacls.exe $dirstring /inheritancelevel:e /remove:g $targetdenyobjectstring /T /Q /C"
 # Write commands into log file.
 echo $ps1 >>Commandstorun.log;
 echo $ps2 >>Commandstorun.log;

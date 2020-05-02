991e756a7b679134c40562e7cb938eaed8ecd60f
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-05-02 01:20:56 -0400

    Update grant_deny.sh

--- grant_deny.sh
+++ grant_deny.sh
@@ -67,13 +67,13 @@ var+="$1"
 var+="$2"
 var+="$quote"
 echo "Taking owner ship of the file/directory $var";
-ps1="start /REALTIME powershell Invoke-Expression \"takeown.exe /D Y /A /R /F' $var\""
+ps1="powershell Invoke-Expression \"takeown.exe /D Y /A /R /F' $var\""
 eval $ps1;
 echo "Setting ownership to $grantuser on $var";
-ps2="start /REALTIME powershell Invoke-Expression \"icacls.exe $var /setowner $grantuser /T /Q /C\""
+ps2="powershell Invoke-Expression \"icacls.exe $var /setowner $grantuser /T /Q /C\""
 eval $ps2;
 echo "Giving Full permission to: $grantuser and denying $denyuser in: $var with container inheritance"
-ps3="start /REALTIME powershell Invoke-Expression \"icacls.exe $var /inheritance:r /grant:r $(setstr $grantuser) /remove:g $(setstr $denyuser) /deny $(setstr $denyuser) /T /Q /C\""
+ps3="powershell Invoke-Expression \"icacls.exe $var /inheritance:r /grant:r $(setstr $grantuser) /remove:g $(setstr $denyuser) /deny $(setstr $denyuser) /T /Q /C\""
 eval $ps3;
 }
 echo "Listing files and folders in $directory"

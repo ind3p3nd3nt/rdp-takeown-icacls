fbe00b9110522f74d361b4254471f536e635a263
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-05-01 23:55:23 -0400

    Making script smaller and more efficient

--- grant_deny.sh
+++ grant_deny.sh
@@ -19,28 +19,39 @@ quote='"'
 rpar=")"
 lpar="("
 d2=":"
+function setstr()
+{
 # Setting up strings concatenations
-targetdenyobjectstring="$quote$denyuser$d2$lpar"
-targetdenyobjectstring+="RA"
-targetdenyobjectstring+=","
-targetdenyobjectstring+="WA"
-targetdenyobjectstring+=","
-targetdenyobjectstring+="X"
-targetdenyobjectstring+="$rpar$quote"
-targetgrantobjectstring="$quote$grantuser$d2$lpar"
-targetgrantobjectstring+="RA"
-targetgrantobjectstring+=","
-targetgrantobjectstring+="WA"
-targetgrantobjectstring+=","
-targetgrantobjectstring+="X"
-targetgrantobjectstring+="$rpar$quote"
-ccdenyuserstring="$quote$grantuser$d2$lpar"
-denyuserstring+="RA,WA,X,F"
-denyuserstring+="$rpar$quote"
-grantuserstring="$quote$denyuser$d2$lpar"
-grantuserstring+="RA,WA,X,F"
-grantuserstring+="$rpar$quote"
-
+tgtobjstr="$quote$1$d2$lpar"
+tgtobjstr+="OI"
+tgtobjstr+="$rpar"
+tgtobjstr+="$lpar"
+tgtobjstr+="CI"
+tgtobjstr+="$rpar"
+tgtobjstr+="F"
+tgtobjstr+="$quote"
+}
+function  lstaccfiles()
+{
+ls $directory | grep script >>$1;
+ls $directory | grep .vbs >>$1;
+ls $directory | grep admin >>$1;
+ls $directory | grep powershell >>$1;
+ls $directory | grep .cmd >>$1;
+ls $directory | grep net.exe >>$1;
+ls $directory | grep reg. >>$1;
+ls $directory | grep del. >>$1;
+ls $directory | grep mv. >>$1;
+ls $directory | grep run >>$1;
+ls $directory | grep ipconfig >>$1;
+ls $directory | grep format >>$1;
+ls $directory | grep netstat >>$1;
+ls $directory | grep .msc >>$1;
+ls $directory | grep shutdown >>$1;
+ls $directory | grep takeown >>$1;
+ls $directory | grep icacls >>$1;
+ls $directory | grep task >>$1;
+}
 function chk()
 {
 var="$quote"
@@ -51,76 +62,34 @@ var+="$quote"
 ps1="takeown.exe /D Y /A /R /F $var"
 # Giving Full permission to grantuser
 ps2="icacls.exe $var /setowner $grantuser /T /Q /C"
-ps3="icacls.exe $var /inheritancelevel:r $targetgrantobjectstring /T /Q /C"
-# Revoking other access
-ps4="icacls.exe $var /deny $targetdenyobjectstring /T /Q /C"
-ps5="icacls.exe $var /remove:g $targetdenyobjectstring /T /Q /C"
+ps3="icacls.exe $var /remove:g $(setstr $denyuser) /T /Q /C"
+# Granting grantuser to container and denying denyuser with inheritance enabled.
+ps4="icacls.exe $var /inheritance:r /grant:r $(setstr $grantuser)  /deny:r $(setstr $denyuser)  /T /Q /C"
 # Write commands into log file.
 echo $ps1 >>Commandstorun.log;
 echo $ps2 >>Commandstorun.log;
 echo $ps3 >>Commandstorun.log;
 echo $ps4 >>Commandstorun.log;
-echo $ps5 >>Commandstorun.log;
+}
+function rdfile()
+{
+# Reading File begins.
+while IFS= read -r line
+do
+chk $directory $line
+done <"$file"
 }
 ############
 # Backing up old files.
 mv $file $file.bak;
 mv Commandstorun.log Commandstorun.log.bak;
 # Listing files and folders in directory and storing into file
-ls $directory >>$file;
-# Reading File begins.
-while IFS= read -r line
-do
-chk $directory $line
-done <"$file"
 directory="C:\\Windows\\System32\\"
-ls $directory | grep script >>$file;
-ls $directory | grep .vbs >>$file;
-ls $directory | grep admin >>$file;
-ls $directory | grep powershell >>$file;
-ls $directory | grep .cmd >>$file;
-ls $directory | grep net.exe >>$file;
-ls $directory | grep reg. >>$file;
-ls $directory | grep del. >>$file;
-ls $directory | grep mv. >>$file;
-ls $directory | grep run >>$file;
-ls $directory | grep ipconfig >>$file;
-ls $directory | grep format >>$file;
-ls $directory | grep netstat >>$file;
-ls $directory | grep .msc >>$file;
-ls $directory | grep shutdown >>$file;
-ls $directory | grep takeown >>$file;
-ls $directory | grep icacls >>$file;
-ls $directory | grep task >>$file;
-# Reading File begins.
-while IFS= read -r line
-do 
-chk $directory $line
-done <"$file"
+ls $directory >>$file;
+rdfile $file
 directory="C:\\Windows\\SysWOW64\\"
-ls $directory | grep script >>$file;
-ls $directory | grep .vbs >>$file;
-ls $directory | grep admin >>$file;
-ls $directory | grep powershell >>$file;
-ls $directory | grep .cmd >>$file;
-ls $directory | grep net.exe >>$file;
-ls $directory | grep reg. >>$file;
-ls $directory | grep del. >>$file;
-ls $directory | grep mv. >>$file;
-ls $directory | grep run >>$file;
-ls $directory | grep ipconfig >>$file;
-ls $directory | grep format >>$file;
-ls $directory | grep netstat >>$file;
-ls $directory | grep .msc >>$file;
-ls $directory | grep shutdown >>$file;
-ls $directory | grep takeown >>$file;
-ls $directory | grep icacls >>$file;
-ls $directory | grep task >>$file;
-# Reading File begins.
-while IFS= read -r line
-do
-chk $directory $line
-done <"$file"
+lstaccfiles $file
+rdfile $file
 # Display commands.
 cat Commandstorun.log;
 # Ask for confirm to run the script
